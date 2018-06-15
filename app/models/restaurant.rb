b962bfa2d5341b34de11a6812505ed887ec118e9
jarretbryan <jarret.bryan@gmail.com>
2018-06-15 10:04:14 -0400

    tested basic methods

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,32 @@
 class Restaurant
   attr_accessor :name
+  @@all = []
+
+  def self.all
+    @@all
+  end
 
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+
+  def self.find_by_name(name)
+    Restaurant.all.select do |rest|
+      rest.name == name
+    end
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def customers
+    self.reviews.map do |review|
+      review.customer
+    end
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
-end
 
+  attr_reader :customer, :restaurant
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize (restaurant, customer, review)
+    @restaurant = restaurant
+    @customer = customer
+    @review = review
+    self.class.all << self
+  end
+
+
+end
