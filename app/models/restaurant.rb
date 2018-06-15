2864862839205bbae08563c104fb4b92233a2364
DevinPierce <devinmpierce@gmail.com>
2018-06-15 10:19:37 -0400

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
 class Restaurant
+  
   attr_accessor :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|r| r.name == name}
+  end
+
+  def reviews
+    Review.all.select {|rv| rv.restaurant == self}
+  end
+
+  def customers
+    reviews.map {|rv| rv.customer}.uniq # I don't think customers should be able to leave multiple reviews for the same restaurant but just in case
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
-end
 
+  attr_accessor :customer, :restaurant, :content
+
+  @@all = []
+
+  def initialize(restaurant:, customer:, content:)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
