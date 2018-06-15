78ca37c02f859dc66be9e2762480aa8a7b64e200
github username <github email address>
2018-06-15 10:41:19 -0400

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,33 @@
 class Restaurant
+
+  @@all = []
+
   attr_accessor :name
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def find_by_name
+    self.all.find do |name|
+      name == full_name
+    end
+  end
+
+  def reviews
+    Review.all
+  end
+
+  def customers
+    reviews.select do |review|
+      review.restaurant == self.name
+      review.customer
+    end
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,25 @@
 class Review
-  
-end
 
+  @@all = []
+
+  attr_reader :restaurant, :customer
+
+  def initialize(restaurant, customer)
+    @customer = customer
+    @restaurant = restaurant
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    @customer
+  end
+
+  def restaurant
+    @restaurant
+  end
+
+end
