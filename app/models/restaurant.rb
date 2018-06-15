c01dd58388972c721dda95a0f9befca1f32dfc66
gjbonner <gjbonner@gmail.com>
2018-06-15 10:35:52 -0400

    challenge submitted

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,31 @@
 class Restaurant
   attr_accessor :name
 
+  @@all = []
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(restaurant_name)
+    Restaurant.all.find do |restaurant|
+      restaurant.name == restaurant_name
+    end
+  end
+
+  def reviews
+    Restaurant.all.select do |restaurant|
+      restaurant.reviews
+    end
+  end
+
+  def customers
+   this_restaurant = Restaurant.all.select {|restaurant| restaurant.name == self}
+    this_restaurant.customer
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,11 @@ def reload
   load 'config/environment.rb'
 end
 
+
 Pry.start
+
+giles = Customer.new("Giles", "Bonner")
+restaurant = Restaurant.new("my restaurant")
+Customer.find_by_name("Giles Bonner")
+
+giles.add_review(restaurant, "good food!")
