a9b865038e6f9fcff32d722a462c2f7563afbdaa
Shing Wong <shing2487@gmail.com>
2018-06-15 10:38:15 -0400

    done

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,19 @@
 class Review
-  
-end
 
+  attr_reader :customer, :restaurant
+  attr_accessor :content
+
+  @@all = []
+
+  def initialize(customer, restaurant, content=nil)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+end
