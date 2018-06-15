f7fdbdb83901a9b5b3646d44cfdf0127aa4f8d67
KoichiSakamoto <isaac.fiore@mail.mcgill.ca>
2018-06-15 10:38:22 -0400

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,32 @@
 class Restaurant
+  @@all = []
+
   attr_accessor :name
+
+  #Instance Methods
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    self.reviews.collect {|review| review.customer}
+  end
+
+  #Class Methods
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(string)
+    all.detect {|restaurant| restaurant.name == string}
+  end
+
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,23 @@
 class Review
-  
-end
 
+  @@all = []
+
+  attr_accessor :content
+  attr_reader :customer, :restaurant
+
+  #Instance Methods
+
+  def initialize(customer, restaurant, content)
+    @content = content
+    @customer = customer
+    @restaurant = restaurant
+    @@all << self
+  end
+
+  #Class Methods
+
+  def self.all
+    @@all
+  end
+
+end
