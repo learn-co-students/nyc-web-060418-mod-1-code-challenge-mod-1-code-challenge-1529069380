bf64c8021553d1c5f1806c62ef7b3eb36e5acf84
github username <github email address>
2018-06-15 10:10:06 -0400

    finished and data entries sanitized

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,31 @@
 class Restaurant
   attr_accessor :name
 
+  @@all_restaurants = []
+
   def initialize(name)
     @name = name
+    self.class.all << self
+  end
+
+  # instance methods
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.collect {|review| review.customer}
+  end
+
+  # class methods
+
+  def self.all
+    @@all_restaurants
+  end
+
+  def self.find_by_name(name)
+    all.find {|restaurant| restaurant.name.downcase == name}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,9 @@ def reload
   load 'config/environment.rb'
 end
 
-Pry.start
+customer1 = Customer.new("Michael", "O'Brien")
+customer2 = Customer.new("Jackson", "Sennett")
+customer3 = Customer.new("Michael", "Jackson")
+restaurant1 = Restaurant.new("Royal Pizza")
+restaurant2 = Restaurant.new("Drunken Dumpling")
+binding.pry
